bba5bbb03e7ef7ab0e0097665e7f6309025c00a6
Phil Wang <lucidrains@gmail.com>
2020-06-07 21:49:07 -0700

    move efficient image attention to another repo

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
   name = 'stylegan2_pytorch',
   packages = find_packages(),
   scripts=['bin/stylegan2_pytorch'],
-  version = '0.10.1',
+  version = '0.10.2',
   license='GPLv3+',
   description = 'StyleGan2 in Pytorch',
   author = 'Phil Wang',
@@ -21,7 +21,8 @@ setup(
       'torchvision',
       'pillow',
       'torch_optimizer',
-      'contrastive_learner>=0.1.0'
+      'contrastive_learner>=0.1.0',
+      'linear_attention_transformer'
   ],
   classifiers=[
       'Development Status :: 4 - Beta',
--- stylegan2_pytorch/stylegan2_pytorch.py
+++ stylegan2_pytorch/stylegan2_pytorch.py
@@ -21,6 +21,7 @@ from torch.autograd import grad as torch_grad
 import torchvision
 from torchvision import transforms
 
+from linear_attention_transformer import ImageLinearAttention
 from contrastive_learner import ContrastiveLearner
 
 from PIL import Image
@@ -249,38 +250,6 @@ class Rezero(nn.Module):
     def forward(self, x):
         return self.fn(x) * self.g
 
-class EfficientAttention(nn.Module):
-    def __init__(self, chan, key_dim = 64, value_dim = 64, heads = 8):
-        super().__init__()
-
-        self.chan = chan
-        self.key_dim = key_dim
-        self.value_dim = value_dim
-        self.heads = heads
-        
-        self.to_q = nn.Conv2d(chan, key_dim * heads, 1)
-        self.to_k = nn.Conv2d(chan, key_dim, 1)
-        self.to_v = nn.Conv2d(chan, value_dim, 1)
-        self.to_out = nn.Conv2d(value_dim * heads, chan, 1)
-
-    def forward(self, x):
-        b, _, h, w = x.shape
-
-        q, k, v = (self.to_q(x), self.to_k(x), self.to_v(x))
-
-        q = q.reshape(b, self.heads, -1, h * w)
-        k = k.reshape(b, -1, h * w)
-        v = v.reshape(b, -1, h * w)
-
-        k = k.softmax(dim=2)
-        q = q.softmax(dim=2)
-
-        context = torch.einsum('bdn,ben->bde', k, v)
-        out = torch.einsum('bhdn,bde->bhen', q, context)
-        out = out.reshape(b, -1, h, w)
-        out = self.to_out(out)
-        return out
-
 # stylegan2 classes
 
 class StyleVectorizer(nn.Module):
@@ -474,7 +443,7 @@ class Discriminator(nn.Module):
             blocks.append(block)
 
             attn_fn = nn.Sequential(*[
-                Residual(Rezero(EfficientAttention(out_chan))) for _ in range(2)
+                Residual(Rezero(ImageLinearAttention(out_chan))) for _ in range(2)
             ]) if num_layer in attn_layers else None
 
             attn_blocks.append(attn_fn)
