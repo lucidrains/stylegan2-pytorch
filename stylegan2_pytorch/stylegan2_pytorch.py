7131ff118537fa2279471e2cd57f26d4da8cd6a2
Alexandre LE FOURNER <49434349+alexandrelefourner@users.noreply.github.com>
2020-06-26 23:04:17 +0200

    Update stylegan2_pytorch.py
    Fixing indentation error.

--- stylegan2_pytorch/stylegan2_pytorch.py
+++ stylegan2_pytorch/stylegan2_pytorch.py
@@ -529,7 +529,7 @@ class StyleGAN2(nn.Module):
         self.GE = Generator(image_size, latent_dim, network_capacity, transparent = transparent, attn_layers = attn_layers, no_const = no_const)
 
         # experimental contrastive loss discriminator regularization
-		cl_dimensions = 4 if transparent else 3
+        cl_dimensions = 4 if transparent else 3
         self.D_cl = ContrastiveLearner(self.D, image_size, hidden_layer='flatten', channels_nb = cl_dimensions) if cl_reg else None
 
         # wrapper for augmenting all images going into the discriminator
