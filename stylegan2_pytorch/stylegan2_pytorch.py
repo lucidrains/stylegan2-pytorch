c74e436ab8d68f7b39240b95649c90942b4eca18
Phil Wang <lucidrains@gmail.com>
2020-06-26 17:07:31 -0700

    make sure contrastive loss regularization and transparent images are not both turned on

--- stylegan2_pytorch/stylegan2_pytorch.py
+++ stylegan2_pytorch/stylegan2_pytorch.py
@@ -529,6 +529,7 @@ class StyleGAN2(nn.Module):
         self.GE = Generator(image_size, latent_dim, network_capacity, transparent = transparent, attn_layers = attn_layers, no_const = no_const)
 
         # experimental contrastive loss discriminator regularization
+        assert not (transparent and cl_reg), 'contrastive loss regularization does not work with transparent images yet'
         self.D_cl = ContrastiveLearner(self.D, image_size, hidden_layer='flatten') if cl_reg else None
 
         # wrapper for augmenting all images going into the discriminator
