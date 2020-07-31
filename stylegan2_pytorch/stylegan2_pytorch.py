a58723dfc66fee113c1984e236593eff8f32dcd7
Phil Wang <lucidrains@gmail.com>
2020-07-30 22:57:40 -0700

    wait a bit before doing path length regularization and gradient penalty

--- stylegan2_pytorch/stylegan2_pytorch.py
+++ stylegan2_pytorch/stylegan2_pytorch.py
@@ -699,8 +699,8 @@ class Trainer():
 
         aug_prob   = self.aug_prob
 
-        apply_gradient_penalty = self.steps % 4 == 0
-        apply_path_penalty = self.steps % 32 == 0
+        apply_gradient_penalty = self.steps > 5000 and self.steps % 4 == 0
+        apply_path_penalty = self.steps > 5000 and self.steps % 32 == 0
         apply_cl_reg_to_generated = self.steps > 20000
 
         backwards = partial(loss_backwards, self.fp16)
