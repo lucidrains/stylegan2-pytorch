696d0cb18d7cc7995c40c2bfa3e3bc29ec20decc
Phil Wang <lucidrains@gmail.com>
2020-08-10 14:41:09 -0700

    add support for 2 channeled images

--- stylegan2_pytorch/stylegan2_pytorch.py
+++ stylegan2_pytorch/stylegan2_pytorch.py
@@ -206,10 +206,29 @@ def convert_transparent_to_rgb(image):
     return image
 
 class expand_greyscale(object):
-    def __init__(self, num_channels):
-        self.num_channels = num_channels
+    def __init__(self, transparent):
+        self.transparent = transparent
+
     def __call__(self, tensor):
-        return tensor.expand(self.num_channels, -1, -1)
+        channels = tensor.shape[0]
+        num_target_channels = 4 if self.transparent else 3
+
+        if channels == num_target_channels:
+            return tensor
+
+        alpha = None
+        if channels == 1:
+            color = tensor.expand(3, -1, -1)
+        elif channels == 2:
+            color = tensor[:1].expand(3, -1, -1)
+            alpha = tensor[1:]
+        else:
+            raise Exception(f'image with invalid number of channels given {channels}')
+
+        if alpha is None and self.transparent:
+            alpha = torch.ones(1, *tensor.shape[1:], device=tensor.device)
+
+        return color if not self.transparent else torch.cat((color, alpha))
 
 def resize_to_minimum_size(min_size, image):
     if max(*image.size) < min_size:
@@ -232,7 +251,7 @@ class Dataset(data.Dataset):
             transforms.Resize(image_size),
             RandomApply(aug_prob, transforms.RandomResizedCrop(image_size, scale=(0.5, 1.0), ratio=(0.98, 1.02)), transforms.CenterCrop(image_size)),
             transforms.ToTensor(),
-            transforms.Lambda(expand_greyscale(num_channels))
+            transforms.Lambda(expand_greyscale(transparent))
         ])
 
     def __len__(self):
