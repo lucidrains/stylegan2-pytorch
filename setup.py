104591c7fe41d2d5bb92bf21895d9159fc0f0306
Phil Wang <lucidrains@gmail.com>
2020-09-26 23:41:23 -0700

    move addition of blur to v1.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
           'stylegan2_pytorch = stylegan2_pytorch.cli:main',
       ],
   },
-  version = '1.0.2',
+  version = '1.0.4',
   license='GPLv3+',
   description = 'StyleGan2 in Pytorch',
   author = 'Phil Wang',
