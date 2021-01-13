6a37a77f01f3ccb0da2872b256c3b906ad29c48f
Phil Wang <lucidrains@gmail.com>
2021-01-13 11:23:49 -0800

    fix an issue with new version of pytorch fid

--- stylegan2_pytorch/stylegan2_pytorch.py
+++ stylegan2_pytorch/stylegan2_pytorch.py
@@ -1145,7 +1145,7 @@ class Trainer():
             for j in range(generated_images.size(0)):
                 torchvision.utils.save_image(generated_images[j, :, :, :], str(Path(fake_path) / f'{str(j + batch_num * self.batch_size)}-ema.{ext}'))
 
-        return fid_score.calculate_fid_given_paths([real_path, fake_path], 256, True, 2048)
+        return fid_score.calculate_fid_given_paths([real_path, fake_path], 256, latents.device, 2048)
 
     @torch.no_grad()
     def truncate_style(self, tensor, trunc_psi = 0.75):
