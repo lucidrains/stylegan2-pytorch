fc22408b238a2ff5d016468adfdd9c8a53cda5f2
Phil Wang <lucidrains@gmail.com>
2021-08-05 13:17:40 -0700

    fix image attention layernorm

--- stylegan2_pytorch/stylegan2_pytorch.py
+++ stylegan2_pytorch/stylegan2_pytorch.py
@@ -87,7 +87,17 @@ class Residual(nn.Module):
     def forward(self, x):
         return self.fn(x) + x
 
-ChanNorm = partial(nn.InstanceNorm2d, affine = True)
+class ChanNorm(nn.Module):
+    def __init__(self, dim, eps = 1e-5):
+        super().__init__()
+        self.eps = eps
+        self.g = nn.Parameter(torch.ones(1, dim, 1, 1))
+        self.b = nn.Parameter(torch.zeros(1, dim, 1, 1))
+
+    def forward(self, x):
+        std = torch.var(x, dim = 1, unbiased = False, keepdim = True).sqrt()
+        mean = torch.mean(x, dim = 1, keepdim = True)
+        return (x - mean) / (std + self.eps) * self.g + self.b
 
 class PreNorm(nn.Module):
     def __init__(self, dim, fn):
