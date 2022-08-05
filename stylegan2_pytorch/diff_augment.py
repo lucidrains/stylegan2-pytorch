a3539a374a4f21753afb9b1f8dae285f4748d12e
AmirMasoud Nourollah <Nourollah.am@gmail.com>
2022-08-05 21:03:47 +0430

    Change some code to Pythonic way.

--- stylegan2_pytorch/diff_augment.py
+++ stylegan2_pytorch/diff_augment.py
@@ -24,15 +24,20 @@ def rand_brightness(x, scale):
     x = x + (torch.rand(x.size(0), 1, 1, 1, dtype=x.dtype, device=x.device) - 0.5) * scale
     return x
 
+
 def rand_saturation(x, scale):
     x_mean = x.mean(dim=1, keepdim=True)
-    x = (x - x_mean) * (((torch.rand(x.size(0), 1, 1, 1, dtype=x.dtype, device=x.device) - 0.5) * 2.0 * scale) + 1.0) + x_mean
+    x = (x - x_mean) * (
+                ((torch.rand(x.size(0), 1, 1, 1, dtype=x.dtype, device=x.device) - 0.5) * 2.0 * scale) + 1.0) + x_mean
     return x
+
 
 def rand_contrast(x, scale):
     x_mean = x.mean(dim=[1, 2, 3], keepdim=True)
-    x = (x - x_mean) * (((torch.rand(x.size(0), 1, 1, 1, dtype=x.dtype, device=x.device) - 0.5) * 2.0 * scale) + 1.0) + x_mean
+    x = (x - x_mean) * (
+                ((torch.rand(x.size(0), 1, 1, 1, dtype=x.dtype, device=x.device) - 0.5) * 2.0 * scale) + 1.0) + x_mean
     return x
+
 
 def rand_translation(x, ratio=0.125):
     shift_x, shift_y = int(x.size(2) * ratio + 0.5), int(x.size(3) * ratio + 0.5)
@@ -49,11 +54,12 @@ def rand_translation(x, ratio=0.125):
     x = x_pad.permute(0, 2, 3, 1).contiguous()[grid_batch, grid_x, grid_y].permute(0, 3, 1, 2)
     return x
 
+
 def rand_offset(x, ratio=1, ratio_h=1, ratio_v=1):
     w, h = x.size(2), x.size(3)
 
     imgs = []
-    for img in x.unbind(dim = 0):
+    for img in x.unbind(dim=0):
         max_h = int(w * ratio * ratio_h)
         max_v = int(h * ratio * ratio_v)
 
@@ -70,11 +76,14 @@ def rand_offset(x, ratio=1, ratio_h=1, ratio_v=1):
 
     return torch.stack(imgs)
 
+
 def rand_offset_h(x, ratio=1):
     return rand_offset(x, ratio=1, ratio_h=ratio, ratio_v=0)
 
+
 def rand_offset_v(x, ratio=1):
     return rand_offset(x, ratio=1, ratio_h=0, ratio_v=ratio)
+
 
 def rand_cutout(x, ratio=0.5):
     cutout_size = int(x.size(2) * ratio + 0.5), int(x.size(3) * ratio + 0.5)
@@ -92,15 +101,18 @@ def rand_cutout(x, ratio=0.5):
     x = x * mask.unsqueeze(1)
     return x
 
+
 AUGMENT_FNS = {
     'brightness': [partial(rand_brightness, scale=1.)],
     'lightbrightness': [partial(rand_brightness, scale=.65)],
-    'contrast':  [partial(rand_contrast, scale=.5)],
-    'lightcontrast':  [partial(rand_contrast, scale=.25)],
+    'contrast': [partial(rand_contrast, scale=.5)],
+    'lightcontrast': [partial(rand_contrast, scale=.25)],
     'saturation': [partial(rand_saturation, scale=1.)],
     'lightsaturation': [partial(rand_saturation, scale=.5)],
-    'color': [partial(rand_brightness, scale=1.), partial(rand_saturation, scale=1.), partial(rand_contrast, scale=0.5)],
-    'lightcolor': [partial(rand_brightness, scale=0.65), partial(rand_saturation, scale=.5), partial(rand_contrast, scale=0.5)],
+    'color': [partial(rand_brightness, scale=1.), partial(rand_saturation, scale=1.),
+              partial(rand_contrast, scale=0.5)],
+    'lightcolor': [partial(rand_brightness, scale=0.65), partial(rand_saturation, scale=.5),
+                   partial(rand_contrast, scale=0.5)],
     'offset': [rand_offset],
     'offset_h': [rand_offset_h],
     'offset_v': [rand_offset_v],
